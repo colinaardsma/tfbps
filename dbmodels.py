7116eb79b7b48f69ef71608da045faed9da3fe12
Colin Aardsma <colin.aardsma@gmail.com>
2016-10-08 23:01:51 -0500

    got better data extraction, need to split name, team, pos

---
+++ dbmodels.py
@@ -0,0 +1,31 @@
+from google.appengine.ext import db
+
+#define columns of database objects
+class Users(db.Model):
+    username = db.StringProperty(required = True) #sets username to a string and makes it required
+    password = db.StringProperty(required = True) #sets password to a string and makes it required
+    email = db.StringProperty(required = False) #sets email to a string and makes it optional
+    created = db.DateTimeProperty(auto_now_add = True) #sets created to equal date/time of creation (this cannot be modified)
+    last_modified = db.DateTimeProperty(auto_now = True) #sets last_modified to equal current date/time (this can be modified)
+
+#define columns of database objects
+class fantProProj(db.Model):
+    name = db.StringProperty(required = True) #sets title to a string and makes it required
+    team = db.StringProperty(required = True) #sets title to a string and makes it required
+    pos = db.StringProperty(required = True) #sets title to a string and makes it required
+#how to do multiple positions?
+    ab = db.IntegerProperty(required = True)
+    r = db.IntegerProperty(required = True)
+    hr = db.IntegerProperty(required = True)
+    rbi = db.IntegerProperty(required = True)
+    sb = db.IntegerProperty(required = True)
+    avg = db.FloatProperty(required = True)
+    obp = db.FloatProperty(required = True)
+    h = db.IntegerProperty(required = True)
+    double = db.IntegerProperty(required = True)
+    triple = db.IntegerProperty(required = True)
+    bb = db.IntegerProperty(required = True)
+    k = db.IntegerProperty(required = True)
+    slg = db.FloatProperty(required = True)
+    ops = db.FloatProperty(required = True)
+    value = db.FloatProperty(required = False) #change to true later
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import webapp2, urllib2, re, sys
+import webapp2, urllib2, re, sys #built in python classes
+import dbmodels #custom python classes
 
 sys.path.insert(0, 'libs')
-from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulSoup, Tag
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -12,25 +13,44 @@ class MainHandler(webapp2.RequestHandler):
 
         soup = BeautifulSoup(content) #convert html content to searchable BS object
 
+        # for a in soup.select('a'):
+        #     # insert sup tag after the span
+        #     td = soup.new_tag('td')
+        #     td.string = a
+        #     a.insert_after(td)
+        #
+        #     # replace the span tag with it's contents
+        #     a.unwrap()
+
+
+        #.wrap(soup.new_tag("b")) need to wrap player name, pos, team
+
         players = [] #create empty list to hold player data
 
-        trSoup = soup.findAll('tr',attrs={'class':'mpb-available'}) #split data by tr tag with class="mpb-available"
+        trSoup = soup.findAll('tr',attrs={'class':'mpb-available'}) #split data by tr tag with class="mpb-available"; stats per player
 
         for tr in trSoup: #iterate over each tr tag in trSoup
-            for td in tr.findAll('td'): #iterate over each td tag in tr
-                [a.extract() for a in td('a',attrs={'href':'#'})] #remove all <a href="#"></a> tags and their contents
-                [small.extract() for small in td('small',attrs={'class':'dl tip'})] #remove all <small class="dl tip"></small> tags and their contents
-                [name.replaceWith(name.renderContents() + ",") for name in td('a',attrs={'class':'player-name'})] #remove all <a class="player-name"></a> tags but keep their contents and add a ","
-                [team.replaceWith(team.renderContents() + ",") for team in td('a')] #remove all <a></a> tags but keep their contents and add a ","
+            for td in tr.findAll('td'): #iterate over each td tag in tr; break columns apart
+                [a.extract() for a in td('a',attrs={'href':'#'})] #remove all <a href="#"></a> tags and their contents; empty tag
+                [small.extract() for small in td('small',attrs={'class':'dl tip'})] #remove all <small class="dl tip"></small> tags and their contents; DL status
+                [name.replaceWith(name.renderContents() + ",") for name in td('a',attrs={'class':'player-name'})] #remove all <a class="player-name"></a> tags but keep their contents and add a ","; player name
+                [name.findAll('a',attrs={'class':'player-name'})] #remove all <a class="player-name"></a> tags but keep their contents and add a ","; player name
+
+                [team.replaceWith(team.renderContents()) for team in td('a')] #remove all <a></a> tags but keep their contents and add a ","; team name
+                [teamPos.replaceWith(teamPos.renderContents().replace(",", "/").replace("(", "").replace(")","").replace(" - ", ",")) for teamPos in td('small')] #remove all <small></small> tags but keep their contents, change "," to "/", change "(" to "", change ")" to "", change " - " to ",",; team and position
+                [div.extract() for div in td('div',attrs={'id':'directions_extension'})] #remove all <div id="directions-extension"></div> tags and their contents; empty div tag
                 players.append(td) #add td to players list
+
+        # players = [i.split(',')[0] for i in players]
+        # pl = [players[x:x+20] for x in range(0, len(players), 20)]
+
+        # player = dbmodels.fantProProj(name=name, team=team, pos=pos, ab=ab, r=r, hr=hr, rbi=rbi, sb=sb, avg=avg, obp=obp, h=h, double=double, triple=triple, bb=bb, k=k, slg=slg, ops=ops)
+
 
         #need to get all of this into a dictionary instead of a list so that i can easily push it into a db
         #use regular expressions to fix team/position (maybe also ", "?)
 
         self.response.write(players)
-
-
-
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
