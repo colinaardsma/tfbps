3e70c7f906d5ffcf5b1c878857a63b78e6969e6e
colinaardsma <colin.aardsma@ryvit.com>
2017-07-25 17:04:11 -0500

    commented out test prints

--- fa_vs_team.py
+++ fa_vs_team.py
@@ -127,10 +127,10 @@ def pitcher_projections():
     sorted_proj = sorted(projections, key=lambda x: x.dollarValue, reverse=True)
     return sorted_proj
 
-import time
-start = time.time()
-print fa_vs_team(5091, "MachadoAboutNothing")
-# print final_standing_projection(5091)
-end = time.time()
-elapsed = end - start
-print "{elapsed:.2f} seconds".format(elapsed=elapsed)
+# import time
+# start = time.time()
+# print fa_vs_team(5091, "MachadoAboutNothing")
+# # print final_standing_projection(5091)
+# end = time.time()
+# elapsed = end - start
+# print "{elapsed:.2f} seconds".format(elapsed=elapsed)
