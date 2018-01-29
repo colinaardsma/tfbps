59aa5b1f5c7a97d85ec1f5ccada7162e5cdc1f87
Colin Aardsma <colin.aardsma@gmail.com>
2018-01-29 11:47:31 -0600

    mas db -> ndb

--- dbmodels.py
+++ dbmodels.py
@@ -6,148 +6,146 @@ from google.appengine.ext import ndb
 
 class Users(ndb.Model):
     """The user model"""
-    username = db.StringProperty(required=True)
-    password = db.StringProperty(required=True)
-    email = db.StringProperty(required=False)
-    created = db.DateTimeProperty(auto_now_add=True)
-    last_modified = db.DateTimeProperty(auto_now=True)
-    authorization = db.StringProperty(required=False)
+    username = ndb.StringProperty(required=True)
+    password = ndb.StringProperty(required=True)
+    email = ndb.StringProperty(required=False)
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    last_modified = ndb.DateTimeProperty(auto_now=True)
+    authorization = ndb.StringProperty(required=False)
 
 class Batter(ndb.Model):
     """The Batter Model"""
     # Descriptive Properties
-    name = db.StringProperty(required=True)
-    team = db.StringProperty(required=True)
-    pos = db.StringProperty(required=True)
-    last_modified = db.DateTimeProperty(auto_now=True)
-    category = db.StringProperty(required=True)
+    name = ndb.StringProperty(required=True)
+    team = ndb.StringProperty(required=True)
+    pos = ndb.StringProperty(required=True)
+    last_modified = ndb.DateTimeProperty(auto_now=True)
+    category = ndb.StringProperty(required=True)
     # Raw Stat Properties
-    atbats = db.IntegerProperty(required=True)
-    runs = db.IntegerProperty(required=True)
-    hrs = db.IntegerProperty(required=True)
-    rbis = db.IntegerProperty(required=True)
-    sbs = db.IntegerProperty(required=True)
-    avg = db.FloatProperty(required=True)
-    ops = db.FloatProperty(required=True)
+    atbats = ndb.IntegerProperty(required=True)
+    runs = ndb.IntegerProperty(required=True)
+    hrs = ndb.IntegerProperty(required=True)
+    rbis = ndb.IntegerProperty(required=True)
+    sbs = ndb.IntegerProperty(required=True)
+    avg = ndb.FloatProperty(required=True)
+    ops = ndb.FloatProperty(required=True)
     # Initial zScore Properties
-    zScoreR = db.FloatProperty
-    zScoreHr = db.FloatProperty
-    zScoreRbi = db.FloatProperty
-    zScoreSb = db.FloatProperty
-    zScoreAvg = db.FloatProperty
-    zScoreOps = db.FloatProperty
+    zScoreR = ndb.FloatProperty()
+    zScoreHr = ndb.FloatProperty()
+    zScoreRbi = ndb.FloatProperty()
+    zScoreSb = ndb.FloatProperty()
+    zScoreAvg = ndb.FloatProperty()
+    zScoreOps = ndb.FloatProperty()
     # Weighted (Multiplied by AB) Properties
-    weightedR = db.FloatProperty
-    weightedHr = db.FloatProperty
-    weightedRbi = db.FloatProperty
-    weightedSb = db.FloatProperty
-    weightedAvg = db.FloatProperty
-    weightedOps = db.FloatProperty
+    weightedR = ndb.FloatProperty()
+    weightedHr = ndb.FloatProperty()
+    weightedRbi = ndb.FloatProperty()
+    weightedSb = ndb.FloatProperty()
+    weightedAvg = ndb.FloatProperty()
+    weightedOps = ndb.FloatProperty()
     # Weighted and RezScored Properties
-    weightedZscoreR = db.FloatProperty
-    weightedZscoreHr = db.FloatProperty
-    weightedZscoreRbi = db.FloatProperty
-    weightedZscoreSb = db.FloatProperty
-    weightedZscoreAvg = db.FloatProperty
-    weightedZscoreOps = db.FloatProperty
+    weightedZscoreR = ndb.FloatProperty()
+    weightedZscoreHr = ndb.FloatProperty()
+    weightedZscoreRbi = ndb.FloatProperty()
+    weightedZscoreSb = ndb.FloatProperty()
+    weightedZscoreAvg = ndb.FloatProperty()
+    weightedZscoreOps = ndb.FloatProperty()
 
 class Pitcher(ndb.Model):
     """The Pitcher Model"""
     # Descriptive Properties
-    name = db.StringProperty(required=True)
-    team = db.StringProperty(required=True)
-    pos = db.StringProperty(required=True)
-    last_modified = db.DateTimeProperty(auto_now=True)
-    category = db.StringProperty(required=True)
+    name = ndb.StringProperty(required=True)
+    team = ndb.StringProperty(required=True)
+    pos = ndb.StringProperty(required=True)
+    last_modified = ndb.DateTimeProperty(auto_now=True)
+    category = ndb.StringProperty(required=True)
     # Raw Stat Properties
-    ips = db.IntegerProperty(required=True)
-    wins = db.IntegerProperty(required=True)
-    saves = db.IntegerProperty(required=True)
-    strikeOuts = db.IntegerProperty(required=True)
-    era = db.FloatProperty(required=True)
-    whip = db.FloatProperty(required=True)
+    ips = ndb.IntegerProperty(required=True)
+    wins = ndb.IntegerProperty(required=True)
+    saves = ndb.IntegerProperty(required=True)
+    strikeOuts = ndb.IntegerProperty(required=True)
+    era = ndb.FloatProperty(required=True)
+    whip = ndb.FloatProperty(required=True)
     # Initial zScore Properties
-    zScoreW = db.FloatProperty
-    zScoreSv = db.FloatProperty
-    zScoreK = db.FloatProperty
-    zScoreEra = db.FloatProperty
-    zScoreWhip = db.FloatProperty
+    zScoreW = ndb.FloatProperty()
+    zScoreSv = ndb.FloatProperty()
+    zScoreK = ndb.FloatProperty()
+    zScoreEra = ndb.FloatProperty()
+    zScoreWhip = ndb.FloatProperty()
     # Weighted (Multiplied by IP) Properties
-    weightedW = db.FloatProperty
-    weightedSv = db.FloatProperty
-    weightedK = db.FloatProperty
-    weightedEra = db.FloatProperty
-    weightedWhip = db.FloatProperty
+    weightedW = ndb.FloatProperty()
+    weightedSv = ndb.FloatProperty()
+    weightedK = ndb.FloatProperty()
+    weightedEra = ndb.FloatProperty()
+    weightedWhip = ndb.FloatProperty()
     # Weighted and RezScored Properties
-    weightedZscoreW = db.FloatProperty
-    weightedZscoreSv = db.FloatProperty
-    weightedZscoreK = db.FloatProperty
-    weightedZscoreEra = db.FloatProperty
-    weightedZscoreWhip = db.FloatProperty
+    weightedZscoreW = ndb.FloatProperty()
+    weightedZscoreSv = ndb.FloatProperty()
+    weightedZscoreK = ndb.FloatProperty()
+    weightedZscoreEra = ndb.FloatProperty()
+    weightedZscoreWhip = ndb.FloatProperty()
 
 #define columns of database objects
 class FPProjB(ndb.Model):
     """The batter model"""
-
-    name = db.StringProperty(required=True)
-    team = db.StringProperty(required=True)
-    pos = db.StringProperty(required=True)
+    name = ndb.StringProperty(required=True)
+    team = ndb.StringProperty(required=True)
+    pos = ndb.StringProperty(required=True)
 #how to do multiple positions?
-    ab = db.IntegerProperty(required=True)
-    r = db.IntegerProperty(required=True)
-    hr = db.IntegerProperty(required=True)
-    rbi = db.IntegerProperty(required=True)
-    sb = db.IntegerProperty(required=True)
-    avg = db.FloatProperty(required=True)
-    obp = db.FloatProperty(required=True)
-    h = db.IntegerProperty(required=True)
-    double = db.IntegerProperty(required=True)
-    triple = db.IntegerProperty(required=True)
-    bb = db.IntegerProperty(required=True)
-    k = db.IntegerProperty(required=True)
-    slg = db.FloatProperty(required=True)
-    ops = db.FloatProperty(required=True)
-    sgp = db.FloatProperty(required=False) #change to true later
-    zr = db.FloatProperty(required=False)
-    zhr = db.FloatProperty(required=False)
-    zrbi = db.FloatProperty(required=False)
-    zsb = db.FloatProperty(required=False)
-    zops = db.FloatProperty(required=False)
-    last_modified = db.DateTimeProperty(auto_now = True)
-    category = db.StringProperty(required=True)
+    ab = ndb.IntegerProperty(required=True)
+    r = ndb.IntegerProperty(required=True)
+    hr = ndb.IntegerProperty(required=True)
+    rbi = ndb.IntegerProperty(required=True)
+    sb = ndb.IntegerProperty(required=True)
+    avg = ndb.FloatProperty(required=True)
+    obp = ndb.FloatProperty(required=True)
+    h = ndb.IntegerProperty(required=True)
+    double = ndb.IntegerProperty(required=True)
+    triple = ndb.IntegerProperty(required=True)
+    bb = ndb.IntegerProperty(required=True)
+    k = ndb.IntegerProperty(required=True)
+    slg = ndb.FloatProperty(required=True)
+    ops = ndb.FloatProperty(required=True)
+    sgp = ndb.FloatProperty(required=False) #change to true later
+    zr = ndb.FloatProperty(required=False)
+    zhr = ndb.FloatProperty(required=False)
+    zrbi = ndb.FloatProperty(required=False)
+    zsb = ndb.FloatProperty(required=False)
+    zops = ndb.FloatProperty(required=False)
+    last_modified = ndb.DateTimeProperty(auto_now = True)
+    category = ndb.StringProperty(required=True)
 
 #define columns of database objects
 class FPProjP(ndb.Model):
     """# This is the Pitcher Model"""
-
-    name = db.StringProperty(required=True)
-    team = db.StringProperty(required=True)
-    pos = db.StringProperty(required=True)
-#how to do multiple positions?
-    ip = db.FloatProperty(required=True)
-    k = db.IntegerProperty(required=True)
-    w = db.IntegerProperty(required=True)
-    sv = db.IntegerProperty(required=True)
-    era = db.FloatProperty(required=True)
-    whip = db.FloatProperty(required=True)
-    er = db.IntegerProperty(required=True)
-    h = db.IntegerProperty(required=True)
-    bb = db.IntegerProperty(required=True)
-    hr = db.IntegerProperty(required=True)
-    g = db.IntegerProperty(required=True)
-    gs = db.IntegerProperty(required=True)
-    l = db.IntegerProperty(required=True)
-    cg = db.IntegerProperty(required=True)
-    sgp = db.FloatProperty(required=False) #change to true later
-    z_score = db.FloatProperty(required=False) #change to true later
-    last_modified = db.DateTimeProperty(auto_now=True)
-    category = db.StringProperty(required=True)
+    name = ndb.StringProperty(required=True)
+    team = ndb.StringProperty(required=True)
+    pos = ndb.StringProperty(required=True)
+    #how to do multiple positions?
+    ip = ndb.FloatProperty(required=True)
+    k = ndb.IntegerProperty(required=True)
+    w = ndb.IntegerProperty(required=True)
+    sv = ndb.IntegerProperty(required=True)
+    era = ndb.FloatProperty(required=True)
+    whip = ndb.FloatProperty(required=True)
+    er = ndb.IntegerProperty(required=True)
+    h = ndb.IntegerProperty(required=True)
+    bb = ndb.IntegerProperty(required=True)
+    hr = ndb.IntegerProperty(required=True)
+    g = ndb.IntegerProperty(required=True)
+    gs = ndb.IntegerProperty(required=True)
+    l = ndb.IntegerProperty(required=True)
+    cg = ndb.IntegerProperty(required=True)
+    sgp = ndb.FloatProperty(required=False) #change to true later
+    z_score = ndb.FloatProperty(required=False) #change to true later
+    last_modified = ndb.DateTimeProperty(auto_now=True)
+    category = ndb.StringProperty(required=True)
 
 #define columns of database objects
 class Blog(ndb.Model):
-    title = db.StringProperty(required=True)
-    body = db.TextProperty(required=True)
-    created = db.DateTimeProperty(auto_now_add=True)
-    last_modified = db.DateTimeProperty(auto_now=True)
-    author = db.ReferenceProperty(Users, required=True)
-    coords = db.GeoPtProperty(required=False) #store coordinates of user based on URL, not required as it may not always be available
+    title = ndb.StringProperty(required=True)
+    body = ndb.TextProperty(required=True)
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    last_modified = ndb.DateTimeProperty(auto_now=True)
+    author = ndb.ReferenceProperty(Users, required=True)
+    coords = ndb.GeoPtProperty(required=False) #store coordinates of user based on URL, not required as it may not always be available
